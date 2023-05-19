35579a7d139313de84731b585d95714ac16ff103
binhstatic <binhnv@MECORP.LOCAL>
2023-05-19 11:36:10 +0700

    feat: sidebar, navigation and layout

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import { toast } from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/Input';
@@ -14,8 +15,16 @@ import AuthSocialButton from './AuthSocialButton';
 type Variant = 'LOGIN' | 'REGISTER';
 
 export default function AuthForm() {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/users');
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
@@ -44,6 +53,7 @@ export default function AuthForm() {
       // Axios Register
       axios
         .post('/api/register', data)
+        .then(() => signIn('credentials', data))
         .catch(() => toast.error('Something went wrong!'))
         .finally(() => setIsLoading(false));
     }
@@ -60,6 +70,7 @@ export default function AuthForm() {
           }
           if (callback?.ok && !callback?.error) {
             toast.success('Logged in!');
+            router.push('/users');
           }
         })
         .finally(() => setIsLoading(false));
---
+++ app/users/layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from '../components/sidebar/Sidebar';
+
+export interface UsersLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function UsersLayout({ children }: UsersLayoutProps) {
+  return (
+    // @ts-expect-error Server Component
+    <Sidebar>
+      <div className='h-full'>{children}</div>
+    </Sidebar>
+  );
+}
